e8e120956e0d0d5f3f0331bbe8e81dba765f9a94
jantapabh <s6035512034@phuket.psu.ac.th>
2020-09-26 21:17:19 +0700

    add commit midlleware

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const graphqlmiddleware = require('express-graphql')
+
 const app = express()
 
-app.use((req , res) => {
-    res.send('Hello world')
-})
+// app.use('/hello',(req , res) => {
+//     res.json({data: 'Hello'})
+// })
+
+
+// app.use('/world',(req , res) => {
+//     res.json({data: 'world0'})
+// })
+
+app.use(graphqlmiddleware({
+    
+}))
 
 app.listen(3000)
